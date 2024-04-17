8939e897a91c925822bc3cccd095d3e3e629cdd6
Skanda <sskanda98@gmail.com>
2024-04-18 00:38:43 +0530

    add candidatesApplied migration

--- backend/job/models.py
+++ backend/job/models.py
@@ -66,3 +66,9 @@ class Job(models.Model):
     lastDate = models.DateTimeField(default=return_date_time)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     createdAt = models.DateTimeField(auto_now_add=True)
+
+class CandidatesApplied(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    resume = models.CharField(max_length=200)
+    appliedAt = models.DateTimeField(auto_now_add=True)
