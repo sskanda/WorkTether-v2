864ee1180be216e4941dc07157a721cb11988cc3
Skanda <sskanda98@gmail.com>
2024-04-18 01:54:48 +0530

    candidates applied to jobs api

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('jobs/new/', views.newJob, name='new_job'),
     path('job/<str:pk>', views.getJob,name = 'job'),
     path('jobs/<str:pk>/update/', views.updateJob, name='update_job'),
+    path('jobs/<str:pk>/apply/', views.applyToJob, name='apply_to_job'),
+    path('me/jobs/applied/', views.getCurrentUserAppliedJobs, name='current_user_applied_jobs'),
 ]
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.utils import timezone
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import *
-from .serializers import JobSerializer
-from .models import Job
+from .serializers import JobSerializer,CandidatesAppliedSerializer
+from .models import Job, CandidatesApplied
 from .filters import JobsFilter
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 # Create your views here.
 
@@ -68,5 +71,47 @@ def updateJob(request, pk):
     job.save()
 
     serializer = JobSerializer(job, many=False)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def applyToJob(request, pk):
+
+    user = request.user
+    job = get_object_or_404(Job, id=pk)
+
+    
+    if job.lastDate < timezone.now():
+        return Response({ 'error': 'You can not apply to this job. Date is over' }, status=status.HTTP_400_BAD_REQUEST)
+
+    alreadyApplied = job.candidatesapplied_set.filter(user=user).exists()
+
+    if alreadyApplied:
+        return Response({ 'error': 'You have already apply to this job.' }, status=status.HTTP_400_BAD_REQUEST)
+
+
+    jobApplied = CandidatesApplied.objects.create(
+        job = job,
+        user = user,
+        resume = "test"
+    )
+
+    return Response({
+        'applied': True,
+        'job_id': jobApplied.id
+    },
+    status=status.HTTP_200_OK
+    )
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getCurrentUserAppliedJobs(request):
+
+    args = { 'user_id': request.user.id }
+
+    jobs = CandidatesApplied.objects.filter(**args)
+
+    serializer = CandidatesAppliedSerializer(jobs, many=True)
 
     return Response(serializer.data)
